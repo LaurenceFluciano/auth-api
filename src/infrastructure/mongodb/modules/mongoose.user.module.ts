61801b5161664c6ffacdcf1fe6f940090e607f72
laurence <laurencefluciano@gmail.cpom>
2025-07-10 21:32:35 -0300

    Fix repository modules

--- src/infrastructure/mongodb/modules/mongoose.user.module.ts
+++ src/infrastructure/mongodb/modules/mongoose.user.module.ts
@@ -1,8 +1,9 @@
 import { Module } from "@nestjs/common";
-import { CreateUserMongoDBRepository } from "../repository/user.create.repository";
-import { USER_CREATOR_REPOSITORY, USER_GETTER_REPOSITORY } from "src/domain/interface/user.repository.ports";
+import { USER_CREATOR_REPOSITORY, USER_GETTER_REPOSITORY, USER_UPDATE_REPOSITORY } from "src/domain/interface/user.repository.ports";
 import { MongooseSchemaModule } from "./mongoose.schema.module";
+import { CreateUserMongoDBRepository } from "../repository/user.create.repository";
 import { GetUserMongoDBRepository } from "../repository/user.getter.repository";
+import { UpdateUserMongoDBRepository } from "../repository/user.update.repository";
 
 @Module({
     imports: [
@@ -16,6 +17,10 @@ import { GetUserMongoDBRepository } from "../repository/user.getter.repository";
         {
             provide: USER_GETTER_REPOSITORY,
             useClass: GetUserMongoDBRepository
+        },
+        {
+            provide: USER_UPDATE_REPOSITORY,
+            useClass: UpdateUserMongoDBRepository
         }
     ],
     exports: [USER_CREATOR_REPOSITORY,USER_GETTER_REPOSITORY]
