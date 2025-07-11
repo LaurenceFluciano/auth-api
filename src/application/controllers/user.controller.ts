794306795a4e218adf187f49a9c2d8bfa284537a
laurence <laurencefluciano@gmail.cpom>
2025-07-11 16:39:41 -0300

    Controller Structure to implement

--- src/application/controllers/user.controller.ts
+++ src/application/controllers/user.controller.ts
@@ -1,9 +1,67 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Get, Patch, Param, HttpCode, Query } from '@nestjs/common';
 import { UserService } from '../services/user.service';
 import { CreateUserDTO } from '../dtos/users/create.user.dto';
+import { GetByCredentialsDTO, GetUserIdDTO } from '../dtos/users/get.user.dto';
+import { PatchUserNameDTO, PatchUserScopesDTO, PatchUserStatusDTO } from '../dtos/users/patch.user.dto';
 
-@Controller("users/")
+
+@Controller("users")
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
+
+  @Post()
+  @HttpCode(201)
+  createUser(
+    @Body() dtoBody: CreateUserDTO
+  )
+  {
+    return "[POST] Must to be Implemeted";
+  }
+
+
+  @Get(":id")
+  getUserById(
+    @Param() dtoParam: GetUserIdDTO
+  )
+  {
+    return "[GET BY ID] Must to be Implemeted";
+  }
+
+
+  @Get("by-credentials")
+  getUserByCredentials(
+    @Query() dtoQuery: GetByCredentialsDTO
+  )
+  {
+    return "[GET BY CREDENTIALS] Must to be Implemeted";
+  }
+
+
+  @Patch(":id")
+  updateUserName(
+    @Param() dtoParam: GetUserIdDTO,
+    @Body() dtoBody: PatchUserNameDTO
+  )
+  {
+    return "[UPDATE NAME] Must to be Implemeted";
+  }
+
+  @Patch(":id/scopes/")
+  updateUserScopes(
+    @Param() dtoParam: GetUserIdDTO,
+    @Body() dtoBody: PatchUserScopesDTO
+  )
+  {
+    return "[UPDATE SCOPES] Must to be Implemeted";
+  }
+
+  @Patch(":id/status/")
+  updateUserStatus(
+    @Param() dtoParam: GetUserIdDTO,
+    @Body() dtoBody: PatchUserStatusDTO
+  )
+  {
+    return "[UPDATE STATUS] Must to be Implemeted";
+  }
 }
